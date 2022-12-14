718957c5c6d264b2858dc732e33d6c7118f6e9d7
marl-ot <marlot.swyat@gmail.com>
2022-12-14 17:07:32 +0300

    aud and teachers

--- e-journal/config.py
+++
@@ -1,3 +0,0 @@
-url_site = "http://studydep.miigaik.ru/index.php"
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'}
-week_list={"1":"Понедельник", "2":"Вторник", "3":"Среда", "4":"Четверг", "5":"Пятница", "6":"Суббота", "7":"Воскресенье"}
